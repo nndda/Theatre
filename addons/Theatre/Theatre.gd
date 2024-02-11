243f0e822fe971c80c68f6c669b1cde74fbabe38
nnda <96333146+nndda@users.noreply.github.com>
2024-02-11 17:22:45 +0800

    Fix plugin's project settings

--- addons/Theatre/Theatre.gd
+++ addons/Theatre/Theatre.gd
@@ -7,19 +7,19 @@ class Config:
 
     static var dialogue_ignored_dir = "theatre/files_and_resources/dialogue/ignored_directories"
     static func get_ignored_directories() -> PackedStringArray:
-        return [".git", ".godot"] + (
+        return PackedStringArray([".git", ".godot"]) + (
             ProjectSettings.get_setting(dialogue_ignored_dir, "addons").split(",", false)
         )
 
 var configs : Array[Array] = [
     [
-        Config.dialogue_save_to_memory, TYPE_BOOL, true, PROPERTY_HINT_NONE,
+        Config.dialogue_save_to_memory, TYPE_BOOL, true, PROPERTY_HINT_NONE, "",
     ],
     [
-        Config.dialogue_save_to_userpath, TYPE_BOOL, true, PROPERTY_HINT_NONE,
+        Config.dialogue_save_to_userpath, TYPE_BOOL, true, PROPERTY_HINT_NONE, "",
     ],
     [
-        Config.dialogue_ignored_dir, TYPE_STRING, "addons", PROPERTY_HINT_NONE,
+        Config.dialogue_ignored_dir, TYPE_STRING, "addons", PROPERTY_HINT_NONE, "",
     ],
 ]
 
@@ -28,7 +28,7 @@ func _enter_tree():
     print("Theatre v%s by nnda" % get_plugin_version())
 
     for config_item in configs:
-        add_config(config_item[0], config_item[1], config_item[2], config_item[3])
+        add_config(config_item[0], config_item[1], config_item[2], config_item[3], config_item[4])
 
     var err := ProjectSettings.save()
     if err != OK:
@@ -42,8 +42,8 @@ func add_config(
         config_name : String,
         type : int,
         default,
-        hint : int = PROPERTY_HINT_NONE,
-        hint_string : String = ""
+        hint : int,
+        hint_string : String
     ) -> void:
     if ProjectSettings.has_setting(config_name):
         print(config_name, " already exist on ProjectSettings")
