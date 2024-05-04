8687f2ef95b94ab36e7b9474c0b3016bfbae00b5
nnda <96333146+nndda@users.noreply.github.com>
2024-05-04 14:48:05 +0800

    Implement update checking
    - Add automatic update check toggle in project settings

--- addons/Theatre/classes/TheatrePlugin.gd
+++ addons/Theatre/classes/TheatrePlugin.gd
@@ -3,17 +3,19 @@
 class_name TheatrePlugin extends EditorPlugin
 
 class Config extends RefCounted:
+    const GENERAL_AUTO_UPDATE := "theatre/general/updates/check_updates_automatically"
     const DEBUG_SHOW_CRAWL_FOLDER := "theatre/debug/log/show_current_crawling_directory"
     const DIALOGUE_IGNORED_DIR := "theatre/resources/dialogue/ignored_directories"
 
     static func init_configs() -> void:
         print("  Initializing configs...")
         for config_item : Array in [
+            [ GENERAL_AUTO_UPDATE, TYPE_BOOL, true, PROPERTY_HINT_NONE, "", ],
             [ DEBUG_SHOW_CRAWL_FOLDER, TYPE_BOOL, false, PROPERTY_HINT_NONE, "", ],
             [ DIALOGUE_IGNORED_DIR, TYPE_STRING, "addons", PROPERTY_HINT_NONE, "", ],
         ]:
             if ProjectSettings.has_setting(config_item[0]):
-                print(config_item[0], " already exist on ProjectSettings")
+                print("    %s already exist on ProjectSettings" % config_item[0])
             else:
                 ProjectSettings.set_setting(config_item[0], config_item[2])
                 ProjectSettings.add_property_info({
@@ -23,11 +25,13 @@ class Config extends RefCounted:
                     "hint_string": config_item[4],
                 })
                 ProjectSettings.set_initial_value(config_item[0], config_item[2])
+                ProjectSettings.set_as_basic(config_item[0], true)
 
         update()
 
     static func remove_configs() -> void:
         for config_item : String in [
+            GENERAL_AUTO_UPDATE,
             DEBUG_SHOW_CRAWL_FOLDER,
             DIALOGUE_IGNORED_DIR,
         ]:
@@ -40,6 +44,8 @@ class Config extends RefCounted:
         if err != OK:
             push_error("Error saving Theatre config: ", err)
 
+var http_update_req : HTTPRequest
+
 var plugin_submenu : PopupMenu = preload(
     "res://addons/Theatre/components/tool_submenu.tscn"
 ).instantiate()
@@ -50,10 +56,22 @@ func _build() -> bool:
     return true
 
 func _enter_tree() -> void:
+    plugin_submenu.hide()
+
     # Initialize Theatre config
     print("🎭 Theatre v%s by nnda" % get_plugin_version())
+
     # Initialize project settings
     Config.init_configs()
+
+    # Initialize update check
+    http_update_req = HTTPRequest.new()
+    http_update_req.timeout = 3.0
+    http_update_req.request_completed.connect(_update_response)
+    add_child(http_update_req)
+
+    if ProjectSettings.get_setting(Config.GENERAL_AUTO_UPDATE, true):
+        update_check()
 
     # Initialize plugin submenu
     plugin_submenu.id_pressed.connect(tool_submenu_id_pressed)
@@ -63,6 +81,9 @@ func _exit_tree() -> void:
     print("🎭 Disabling Theatre...")
     # Clear project settings
     Config.remove_configs()
+
+    # Clear update check
+    http_update_req.queue_free()
 
     # Clear plugin submenu
     plugin_submenu.id_pressed.disconnect(tool_submenu_id_pressed)
@@ -134,4 +155,37 @@ func tool_submenu_id_pressed(id : int) -> void:
             crawl()
         10:
             init_gitignore()
+        5:
+            update_check()
 
+func update_check() -> void:
+    print("  Checking for updates...")
+    http_update_req.request(
+        "https://api.github.com/repos/nndda/Theatre/releases/latest"
+    )
+
+func _update_response(
+    result : int,
+    response_code : int,
+    headers : PackedStringArray,
+    body : PackedByteArray,
+    ) -> void:
+    if response_code != 200:
+        push_error("  Error getting updates: %d" % response_code)
+    else:
+        var json := JSON.new()
+        var err := json.parse(body.get_string_from_utf8())
+
+        if err != OK:
+            push_error("  Error getting updates data: %s" % error_string(err))
+        else:
+            var data : Dictionary = json.get_data() as Dictionary
+            var current_ver := get_plugin_version()
+            if data["tag_name"] == current_ver:
+                print("  Using the latest version: %s" % current_ver)
+            else:
+                print_rich("  New updates available: %s -> %s" % [current_ver,
+                    "[url=%s]%s[/url]" % [
+                        data["html_url"],
+                        data["tag_name"]]
+                    ])
