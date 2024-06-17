cd480ed255c28d64d021b20c020e234a750a2665
nnda <96333146+nndda@users.noreply.github.com>
2024-06-17 23:09:25 +0800

    Add `Debug` sub class

--- addons/Theatre/classes/Theatre.gd
+++ addons/Theatre/classes/Theatre.gd
@@ -2,6 +2,13 @@
 extends Object
 class_name Theatre
 
+class Debug extends RefCounted:
+    static func format_stack(stack_arr : Array[Dictionary], indent : String = "  ") -> String:
+        var output : String = ""
+        for n in stack_arr.size():
+            output += "%s %d: {source}:{line} @ {function}()\n".format(stack_arr[n]) % [indent, n]
+        return output
+
 static var speed_scale : float = 1.0
 
 static var lamg : String = ""
