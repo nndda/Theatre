4a4b885a15c8fa987c9277bacb907c3519246ac7
nnda <96333146+nndda@users.noreply.github.com>
2024-06-23 13:24:04 +0800

    Add some notes & revert movie writer configurations

--- theatre_demo/preview/preview.gd
+++ theatre_demo/preview/preview.gd
@@ -1,7 +1,7 @@
 extends Control
 
-@onready var stage : Stage = $Stage
-@onready var progress_bar : ProgressBar = $PanelContainer/CenterContainer/VBoxContainer/ProgressBar
+@export var stage : Stage
+@export var progress_bar : ProgressBar
 var tree : SceneTree
 
 var dlg := Dialogue.load(
@@ -11,7 +11,9 @@ var dlg_chr_count : int
 
 func _ready() -> void:
     tree = get_tree()
-    DisplayServer.window_set_size($ReferenceRect.size)
+    # NOTE: Optimize resolution to create preview GIF image.
+    # Require display/window/stretch/mode to be set to `disabled`
+    #DisplayServer.window_set_size($ReferenceRect.size)
 
     dlg_chr_count = dlg._strip(stage.variables, true, true).length()
     progress_bar.value = 0
@@ -22,21 +24,27 @@ func _ready() -> void:
         func():
             progress_bar.value += 1
     )
-    stage.dialogue_label.text_rendered.connect(progress_dlg)
-    stage.finished.connect(tree.quit)
+
+    # NOTE: Autoplay Dialogue
+    #stage.dialogue_label.text_rendered.connect(progress_dlg)
+    #stage.finished.connect(tree.quit)
 
     stage.start(dlg)
 
-func progress_dlg(_text : String) -> void:
-    await tree.create_timer(2.8).timeout
-    stage.progress()
-    if stage.get_line() == 1:
-        tree.quit()
+# NOTE: Autoplay Dialogue
+#func progress_dlg(_text : String) -> void:
+    #await tree.create_timer(2.8).timeout
+    #stage.progress()
 
-#func _input(event: InputEvent) -> void:
-    #if event.is_action_pressed(&"ui_accept"):
-        #if stage.is_playing():
-            #stage.progress()
-        #else:
-            #progress_bar.value = 0
-            #stage.start(dlg)
+    # NOTE: Ends demo on specific line
+    #if stage.get_line() == 1:
+        #tree.quit()
+
+# NOTE: Manual Dialogue control
+func _input(event: InputEvent) -> void:
+    if event.is_action_pressed(&"ui_accept"):
+        if stage.is_playing():
+            stage.progress()
+        else:
+            progress_bar.value = 0
+            stage.start(dlg)
