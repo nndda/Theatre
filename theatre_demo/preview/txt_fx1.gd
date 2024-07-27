6adcae851fce0fddbb721559c09061d74a62cddd
nnda <96333146+nndda@users.noreply.github.com>
2024-07-27 22:25:13 +0800

    Match new theme
    And cache second color variable.

--- theatre_demo/preview/txt_fx1.gd
+++ theatre_demo/preview/txt_fx1.gd
@@ -4,11 +4,12 @@ class_name TextEffect1
 
 var bbcode = "fx1"
 
-const COL := Color("#6cacff")
+const COL := Color("#bff1ff")
+var col_fx := COL.darkened(0.2)
 var f : float
 
 func _process_custom_fx(char_fx : CharFXTransform) -> bool:
     f = sin(char_fx.relative_index / 3.15 - char_fx.elapsed_time * 9.25)
-    char_fx.color = COL.lerp(COL.darkened(0.2), f)
+    char_fx.color = COL.lerp(col_fx, f)
     char_fx.offset.y = f * 1.25
     return true
