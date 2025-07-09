133b53d812d2fd0db1522c636f336499572fea18
nnda <96333146+nndda@users.noreply.github.com>
2025-07-09 20:54:02 +0700

    Prefix subclass `Debug` to `TheatreDebug`

--- addons/Theatre/classes/Theatre.gd
+++ addons/Theatre/classes/Theatre.gd
@@ -1,7 +1,7 @@
 @icon("res://addons/Theatre/assets/icons/Theatre.svg")
 extends Node
 
-class Debug extends RefCounted:
+class TheatreDebug extends RefCounted:
     static func format_stack(stack_arr : Array[Dictionary], indent : String = "  ") -> String:
         var output : String = ""
         for n in stack_arr.size():
