bc7096084da031ecd5d383dd20cd4aea960c3637
nnda <96333146+nndda@users.noreply.github.com>
2025-09-15 16:47:08 +0700

    Minor effect tweak & change BBCode to `wavy`

--- theatre_demo/preview/txt_fx1.gd
+++ theatre_demo/preview/txt_fx1.gd
@@ -2,14 +2,14 @@
 extends RichTextEffect
 class_name TextEffect1
 
-var bbcode = "fx1"
+var bbcode = "wavy"
 
 const COL := Color("#8ae5ff")
 var col_fx := COL.darkened(0.2)
 var f : float
 
 func _process_custom_fx(char_fx : CharFXTransform) -> bool:
-    f = sin(char_fx.relative_index / 3.15 - char_fx.elapsed_time * 9.25)
+    f = sin(char_fx.relative_index / 2.8 - char_fx.elapsed_time * 7.25)
     char_fx.color = COL.lerp(col_fx, f)
     char_fx.offset.y = f * 1.25
     return true
