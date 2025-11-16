0043eb62bcac4b469270cf1f4cf1304bf974dac4
nnda <96333146+nndda@users.noreply.github.com>
2025-11-17 00:37:34 +0700

    Correct highlighting for nested scope property

--- addons/Theatre/classes/DialogueSyntaxHighlighter.gd
+++ addons/Theatre/classes/DialogueSyntaxHighlighter.gd
@@ -12,6 +12,7 @@ const HASH := DialogueParser.HASH
 const EQUALS := "="
 
 const __NAME := "name"
+const __PATH := "path"
 const __TAG := "tag"
 const __ARG := "arg"
 const __SCOPE := "scope"
@@ -128,10 +129,10 @@ func _get_line_syntax_highlighting(line : int) -> Dictionary:
 
         if match_func != null:
             dict[match_func.get_start(__SCOPE)] = COL_scope
-            dict[match_func.get_start(__NAME)] = COL_func_name
-            dict[match_func.get_start(__NAME) - 1] = COL_symbol
-            dict[match_func.get_end(__NAME)] = COL_symbol
-            dict[match_func.get_end(__NAME) + 1] = COL_func_args
+            dict[match_func.get_start(__PATH)] = COL_func_name
+            dict[match_func.get_start(__PATH) - 1] = COL_symbol
+            dict[match_func.get_end(__PATH)] = COL_symbol
+            dict[match_func.get_end(__PATH) + 1] = COL_func_args
             dict[match_func.get_end() - 1] = COL_symbol
             dict[match_func.get_end()] = COL_base_content
         else:
@@ -143,9 +144,9 @@ func _get_line_syntax_highlighting(line : int) -> Dictionary:
 
         if match_vars != null:
             dict[match_vars.get_start(__SCOPE)] = COL_scope
-            dict[match_vars.get_start(__NAME)] = COL_func_name
-            dict[match_vars.get_start(__NAME) - 1] = COL_symbol
-            dict[match_vars.get_end(__NAME)] = COL_symbol
+            dict[match_vars.get_start(__PATH)] = COL_func_name
+            dict[match_vars.get_start(__PATH) - 1] = COL_symbol
+            dict[match_vars.get_end(__PATH)] = COL_symbol
             dict[match_vars.get_start(__VAL)] = COL_func_args
             dict[match_vars.get_end()] = COL_base_content
         else:
